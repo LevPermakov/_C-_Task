d1d9c62ae616cb1313758d2dd1fb79e72e52d08f
PERLev <perl_55@mail.ru>
2023-03-27 09:57:20 +0500

    Добавлены ДЗ: 60,62. Семинар 8

---
+++ Task60/Program.cs
@@ -0,0 +1,68 @@
+﻿// Задача 60. 
+// Сформируйте трёхмерный массив из неповторяющихся двузначных чисел. 
+// Напишите программу, которая будет построчно выводить массив, 
+// добавляя индексы каждого элемента.
+// Массив размером 2 x 2 x 2
+// Результат:
+// 66(0,0,0) 27(0,0,1) 25(0,1,0) 90(0,1,1)
+// 34(1,0,0) 26(1,0,1) 41(1,1,0) 55(1,1,1)
+
+
+int[,,] Create3dArrayRndInt(int rows, int colums, int depth, int min, int max)
+{
+    int[,,] matrix = new int[rows, colums, depth];
+    if (rows * colums * depth > max - min)
+    {
+        Console.WriteLine("Ошибка! Количество элементов массива превышает количество значений заданного интервала.");
+    }
+    else
+    {
+        // Random rnd = new Random();
+        int value = min;
+        for (int i = 0; i < matrix.GetLength(0); i++)
+        {
+            for (int j = 0; j < matrix.GetLength(1); j++)
+            {
+                for (int k = 0; k < matrix.GetLength(2); k++)
+                {
+                    // matrix[i, j, k] = rnd.Next(min, max + 1);
+                    matrix[i, j, k] = value;
+                    value++;
+                }
+            }
+        }
+    }
+    return matrix;
+}
+
+
+void Print3dArray(int[,,] array3d)
+{
+    for (int i = 0; i < array3d.GetLength(0); i++)
+    {
+        for (int j = 0; j < array3d.GetLength(1); j++)
+        {
+            for (int k = 0; k < array3d.GetLength(2); k++)
+            {
+                Console.WriteLine($"{array3d[i, j, k],3}({i},{j},{k}) ");
+            }
+
+
+        }
+    }
+}
+
+
+
+void Main()
+{
+    int[,,] array3D = Create3dArrayRndInt(3, 4, 7, 10, 99);
+    Console.WriteLine();
+    if (array3D[0,0,0] != 0)
+    {
+        Console.WriteLine("3D массив:");
+        Print3dArray(array3D);
+    }
+}
+
+Main();
---
+++ Task62/Program.cs
@@ -0,0 +1,84 @@
+﻿// Задача 62. Напишите программу, которая заполнит спирально массив 4 на 4.
+// Например, на выходе получается вот такой массив:
+// 01 02 03 04
+// 12 13 14 05
+// 11 16 15 06
+// 10 09 08 07
+
+
+int[,] CreateMatrixSpiralToRight(int rows, int colums, int min)
+{
+    int[,] matrix = new int[rows, colums];
+    int value = min;
+    int iMax = colums;
+    int jMax = rows;
+    int iMin = 0;
+    int jMin = 0;
+    while (value <= rows * colums)
+    {
+        int i = iMin;                               //  i const = iMin
+        for (int jr = jMin; jr < jMax; jr++)        //  j right   от jMin до jMax
+        {
+            matrix[i, jr] = value;
+            value++;
+        }
+        iMin++;
+        int j = jMax - 1;                           //  j const = jMax-1
+        for (int id = iMin; id < iMax; id++)        //  i down 
+        {
+            matrix[id, j] = value;
+            value++;
+        }
+        jMax--;
+        i = iMax - 1;
+        for (int jl = jMax - 1; jl >= jMin; jl--)    //  j left
+        {
+            matrix[i, jl] = value;
+            value++;
+        }
+        iMax--;
+        j = jMin;
+        for (int iup = iMax - 1; iup >= iMin; iup--)     //  i up 
+        {
+            matrix[iup, j] = value;
+            value++;
+        }
+        jMin++;
+    }
+    return matrix;
+}
+
+void PrintMatrix(int[,] matrix)
+{
+    for (int i = 0; i < matrix.GetLength(0); i++)
+    {
+        Console.Write("|");
+        for (int j = 0; j < matrix.GetLength(1); j++)
+        {
+            Console.Write($"{matrix[i, j],5}");
+        }
+        Console.WriteLine("|");
+    }
+}
+
+
+void Main()
+{
+    int[,] matrixA = CreateMatrixSpiralToRight(4, 4, 1);
+
+    Console.WriteLine();
+    Console.WriteLine("Матрица А: ");
+    PrintMatrix(matrixA);
+
+
+    // Console.WriteLine();
+    // Console.WriteLine("Матрица B: ");
+    // PrintMatrix(matrixB);
+
+    // int[,] matrixC = MatrixMultiplication(matrixA, matrixB);
+    // Console.WriteLine();
+    // Console.WriteLine("Произведение матриц - Матрица C: ");
+    // PrintMatrix(matrixC);
+}
+
+Main();
