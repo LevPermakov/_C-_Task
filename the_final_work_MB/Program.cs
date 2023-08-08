13cd2fe392e2e5d15db8ecb3d153e999bf4aed7b
PERLev <perl_55@mail.ru>
2023-08-08 19:58:19 +0500

    Введены исходные данные массива

---
+++ the_final_work_MB/Program.cs
@@ -0,0 +1,32 @@
+﻿Console.Write("Введите количество элементов массива: ");
+int m = Convert.ToInt32(Console.ReadLine());
+string [] stringArray = new string [m];
+
+void array(string [] stringArray)
+{
+  for (int i = 0; i<stringArray.Length; i++)
+  {
+     Console.WriteLine($"Введите {i+1} элемент массива: ");
+     stringArray[i] = Console.ReadLine();
+  }
+}
+
+
+void printA(string [] stringArray)
+{
+    Console.Write("[");
+    for (int i = 0; i<stringArray.Length; i++)
+    {
+    Console.Write($"‘{stringArray[i]}’, ");
+    }
+    Console.Write("]");
+}
+
+void Main()
+{
+    array(stringArray);
+    printA(stringArray);
+}
+
+Main ();
+
