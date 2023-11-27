ce7fb18b14d3c4fe0a5e93711af330795ae208e9
PERLev <perl_55@mail.ru>
2023-11-27 22:46:17 +0500

    phone_book write/read json

--- Task49_Python_Seminar_8/phone_book.py
+++ Task49_Python_Seminar_8/phone_book.py
@@ -7,13 +7,61 @@
 # Аналогично делаю ДЗ по Телефонной Книге (получается, что индивидуально - без группы)
 
 #     Реализую Задание в три этапа:
-#     1. "Ручное" формирование записей тел.кн. (как сделала 3-я группа Семинара 8)
-#     2. Сохранение на диск в формате json
-#     3. Работа с записями тел.кн. через sqlite3 и выделенные функции (методы)
-    
+#     1. "Ручное" формирование записей тел.книги
+#     2. Сохраненить/Восстановить Контакт с диска в формате json
+#     3. Работа с записями тел.кн. через sqlite3 и выделенные блоков в функции(методы)
+
+
+# import sqlite3 as sql
+# conn = sql.connect("phone_book.db")   # установка соединения с БД
+# cur = conn.cursor()                   # создание курсора
+# # запрос на создение Таблицы БД
+# cur.execute("""                       
+#             CREATE TABLE IF NOT EXISTS users
+#             (
+#             id INTEGER PRIMARY KEY,
+#             name TEXT,
+#             surname TEXT
+#             phones [TEXT, TEXT],
+#             email  [TEXT],
+#             birthday TEXT
+#             );
+#             """)
+
+            # phones [TEXT],   # ?
+            # email  [TEXT],   # ?
+        
+
+# cur.execute("INSERT INTO users VALUES (1, 'Лев','Пермяков', [+79222428181, +89222428181], 'perl_55@mail.ru','22.12.1955');")
+# cur.execute("SELECT * FROM users;")    #  - вывести всю информацию
+# print(cur.fetchall())   # [(1, 'Ваня', 'Петров'), (2, 'Сергей', 'Сергеев')]
+
+import json
+
 # contacts = {}
-contacts = {'Lev': {'phones': ['+79222428181', ' +89222428181'], 'email': ['perl_55@mail.ru'], 'birthday': ''}, 
- 'Lev_': {'phones': ['+89222428181'], 'email': ['perl_55@mail.ru'], 'birthday': '22/12/1955'}}
+
+def save():
+    with open("phone_book.json","w",encoding="utf-8") as pb:
+        pb.write(json.dumps(contacts,ensure_ascii=False))
+    print("Все контакты успешно сохранены в файле  phone_book.json")
+
+# def load():
+#     with open("phone_book.json","r",encoding="utf-8") as pb:
+#         contacts=json.load(pb)
+#         print(contacts)                    # ??? если нет этой строки: print(contacts) - упоминание contacts, contacts - не загружаются!
+#     print("Контакты успешно загружены")
+    
+try:
+    # load()
+    with open("phone_book.json","r",encoding="utf-8") as pb:
+        contacts=json.load(pb)
+        print(contacts)                    
+    print("Контакты успешно загружены")
+    
+except:
+    # contacts = {}
+    contacts = {'Lev': {'phones': ['+79222428181', ' +89222428181'], 'email': ['perl_55@mail.ru'], 'birthday': ''}, 
+    'Lev_': {'phones': ['+89222428181'], 'email': ['perl_55@mail.ru'], 'birthday': '22/12/1955'}}
     
 # operations_dict = {
 #                     'add': add_contact(),
@@ -25,14 +73,11 @@ contacts = {'Lev': {'phones': ['+79222428181', ' +89222428181'], 'email': ['perl
 #                     'save': save_contact()
 #                     }
 
-# def add_contact():
-# def save():
-
         # try:
         #     operations_dict[command]
-            
         # except:
         #     print(f'Команда: {command} - не найдена')
+        
 
 def main_cycle():
     while True:
@@ -40,10 +85,9 @@ def main_cycle():
               add - добавить контакт
               del - удалить контакт
               find - поиск контакта
-              show - показать все контакты
-              edit - изменить контакт
-              close - закрыть приложение
+              show - показать все контакт
               save - сохранить контакт
+              edit - закрыть приложение
               '''
             )
         command = input("Введите команду: ")  # Особенность: 1) id - Имя
@@ -61,11 +105,10 @@ def main_cycle():
             # {'Lev': {'phones': [''], 'email': [''], 'birthday': ''}, 
             #  'Lev_': {'phones': ['+89222428181'], 'email': ['perl_55@mail.ru'], 'birthday': '22/12/1955'}, 
             #  '': {'phones': [''], 'email': [''], 'birthday': ''}}
-            
             print("Контакт успешно добавлен")
-            print(contacts)
+            # print(contacts)
             print()
-        elif command == "show":
+        elif command == "show":                       # сделать построчный вывод каждого контакта
             print(contacts)
             print()
         elif command == "find":
@@ -77,10 +120,11 @@ def main_cycle():
             print("Запись успешно удалена")
             print()
         elif command == "save":
-            # save()
-            print("Все контакты успешно сохранены")
+            save()
+            # print("Все контакты успешно сохранены")
             print()
         elif command == "exit":
+            print()
             break
         else:
             print("Некорректная команда.")
